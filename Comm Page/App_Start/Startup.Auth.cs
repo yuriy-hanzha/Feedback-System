b0f0ca049aa8442c6115035a081fbcce37c0e23f
yuriy-hanzha <yurij.ganzha@gmail.com>
2016-04-29 02:17:25 +0300

    Some weird change

--- Comm Page/App_Start/Startup.Auth.cs
+++ Comm Page/App_Start/Startup.Auth.cs
@@ -89,31 +89,6 @@ namespace Comm_Page
                 ClientSecret = "EYQ8-33XLBHHL2JQ6EfmvXS9"
             });
 
-            //app.UseTwitterAuthentication(new TwitterAuthenticationOptions
-            //{
-            //    ConsumerKey = "HsfuhS9F1GmZ4777lGCJqZvse",
-            //    ConsumerSecret = "gIA29tazYztYvf9maMa7HtG278slnqHSnV619lzoGTewtRrrRI",
-            //    BackchannelCertificateValidator = new CertificateSubjectKeyIdentifierValidator(new[]
-            //    {
-            //        "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
-            //        "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
-            //        "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
-            //        "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
-            //        "5168FF90AF0207753CCCD9656462A212B859723B", //DigiCert SHA2 High Assurance Server C‎A 
-            //        "B13EC36903F8BF4701D498261A0802EF63642BC3" //DigiCert High Assurance EV Root CA
-            //    }),
-            //    SignInAsAuthenticationType = DefaultAuthenticationTypes.ExternalCookie,
-            //    Provider = new TwitterAuthenticationProvider()
-            //    {
-            //        OnAuthenticated = (context) =>
-            //        {
-            //            context.Identity.AddClaim(new System.Security.Claims.Claim("urn:twitter:access_token", context.AccessToken, XmlSchemaString, "Twitter"));
-            //            context.Identity.AddClaim(new System.Security.Claims.Claim("urn:tokens:twitter:accesstoken", context.AccessToken));
-            //            context.Identity.AddClaim(new System.Security.Claims.Claim("urn:tokens:twitter:accesstokensecret", context.AccessTokenSecret));
-            //            return Task.FromResult(0);
-            //        }
-            //    }
-            //});
             var twitterOptions = new Microsoft.Owin.Security.Twitter.TwitterAuthenticationOptions()
             {
                 ConsumerKey = "HsfuhS9F1GmZ4777lGCJqZvse",
